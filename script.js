2b7fbac40d2786030ccfb39b2e1adb254d623c66
Joshua <joshuajodrian@gmail.com>
2022-05-02 21:26:12 +0800

    fix playRound function

--- script.js
+++ script.js
@@ -7,34 +7,25 @@ function computerPlay(){
 function playRound(player, computer){
     player.toLowerCase();
     computer.toLowerCase();
-    switch(player, computer){
+    switch(true){
         case (player === "paper" && computer === "paper"):
             return "Draw! You both picked paper";
-        break;
         case (player === "paper" && computer === "rock"):
             return "You win! Paper beats rock";
-        break;
         case (player === "paper" && computer === "scissors"):
             return "You lose! Scissors beats paper";
-        break;
         case (player === "rock" && computer === "paper"):
             return "You lose! Paper beats rock";
-        break;
         case (player === "rock" && computer === "rock"):
             return "Draw! You both picked rock";
-        break;
         case (player === "rock" && computer === "scissors"):
             return "You win! Rock beats scissors";
-        break;
         case (player === "scissors" && computer === "paper"):
             return "You win! Scissors beats paper";
-        break;
         case (player === "scissors" && computer === "rock"):
             return "You lose! Rock beats scissors";
-        break;
         case (player === "scissors" && computer === "scissors"):
             return "Draw! You both picked scissors";
-        break;
     }
 }
 const player = "rock";
